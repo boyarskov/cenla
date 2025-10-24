deeba67a0f21e26a6bbdc3a4209443f824bdf747
boyarskov <arboyarskov@ya.ru>
2025-10-24 21:23:03 +0300

    Refactor structure project

--- src/main/java/task2/task1/Greeting.java
+++ src/main/java/task2/task1/Greeting.java
@@ -1,5 +1,7 @@
+package task2.task1;
+
 public class Greeting {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java");
     }
 }
